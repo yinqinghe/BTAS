13df25d4d33cc1b225e6132c06d9b5a2e02b26ca
殷清贺 <987746808@qq.com>
2026-06-05 15:48:21 +0800

    5.0.5

--- BTAS.js
+++ BTAS.js
@@ -2,7 +2,7 @@
 // @name         BTAS
 // @namespace    https://github.com/yinqinghe/BTAS
 // @homepageURL  https://github.com/yinqinghe/BTAS
-// @version      5.0.4
+// @version      5.0.5
 // @description  Blue Team Auxiliary Script
 // @author       Barry, Jack, Xingyu, Mike
 // @license      Apache-2.0
